ff08d736026a92c4f72a872f845796f66ba12101
Nathan Sobo <nathan@github.com>
2017-07-12 12:07:23 -0600

    Receive and send messages on PeerPool
    ...instead of on individual connection objects

--- test/peer-pool.test.js
+++ test/peer-pool.test.js
@@ -20,52 +20,48 @@ suite('PeerPool', () => {
   })
 
   test('connection and sending messages between peers', async () => {
-    const peer1Pool = new PeerPool({
-      peerId: '1',
-      restGateway: server.restGateway,
-      pubSubGateway: server.pubSubGateway,
-      delegate: {
-        didReceiveIncomingConnection () {}
-      }
-    })
-    await peer1Pool.subscribe()
-
-    let peer2ConnectionToPeer1
-    const peer2Pool = new PeerPool({
-      peerId: '2',
-      restGateway: server.restGateway,
-      pubSubGateway: server.pubSubGateway,
-      delegate: {
-        didReceiveIncomingConnection (connection) {
-          assert.equal(connection.peerId, '1')
-          peer2ConnectionToPeer1 = connection
-        }
-      },
-      fragmentSize: 10
-    })
-    await peer2Pool.subscribe()
+    const peer1Pool = await buildPeerPool('1', server)
+    const peer2Pool = await buildPeerPool('2', server)
+    const peer3Pool = await buildPeerPool('3', server)
 
     // Connection
-    const peer1ConnectionToPeer2 = await peer1Pool.connect('2')
-    await condition(() => peer2ConnectionToPeer1 != null)
+    await peer1Pool.connectTo('3')
+    await peer2Pool.connectTo('3')
+    await condition(() => peer3Pool.isConnectedToPeer('1') && peer3Pool.isConnectedToPeer('2'))
 
-    const peer2Inbox = []
-    peer2ConnectionToPeer1.onReceive((message) => {
-      peer2Inbox.push(message.toString())
+    const peer3Inbox = []
+    peer3Pool.onReceive(({senderId, message}) => {
+      peer3Inbox.push({senderId, message: message.toString()})
+      peer3Inbox.sort((a, b) => a.senderId.localeCompare(b.peerId))
     })
 
     // Single-part messages
-    peer1ConnectionToPeer2.send(Buffer.from('hello'))
-    await condition(() => deepEqual(peer2Inbox, ['hello']))
-    peer2Inbox.length = 0
+    peer1Pool.send('3', Buffer.from('a'))
+    peer2Pool.send('3', Buffer.from('b'))
+
+    await condition(() => deepEqual(peer3Inbox, [
+      {senderId: '1', message: 'a'},
+      {senderId: '2', message: 'b'}
+    ]))
+    peer3Inbox.length = 0
 
     // Multi-part messages
     const longMessage = 'x'.repeat(22)
-    peer1ConnectionToPeer2.send(Buffer.from(longMessage))
-    await condition(() => deepEqual(peer2Inbox, [longMessage]))
-    peer2Inbox.length = 0
+    peer1Pool.send('3', Buffer.from(longMessage))
+    await condition(() => deepEqual(peer3Inbox, [{senderId: '1', message: longMessage}]))
+    peer3Inbox.length = 0
   })
 })
+
+async function buildPeerPool (peerId, server) {
+  const peerPool = new PeerPool({
+    peerId,
+    restGateway: server.restGateway,
+    pubSubGateway: server.pubSubGateway,
+  })
+  await peerPool.subscribe()
+  return peerPool
+}
 
 function condition (fn) {
   return new Promise((resolve) => {
