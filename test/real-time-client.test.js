6d9e57bb69ddf357057ec522f90b041d8ce05bcd
Jason Rudolph <jason@jasonrudolph.com>
2017-09-15 10:33:21 -0400

    ✅ Add unit test for failed attempt to create portal

---
+++ test/real-time-client.test.js
@@ -0,0 +1,24 @@
+require('./setup')
+const assert = require('assert')
+const RealTimeClient = require('../lib/real-time-client')
+
+suite.only('RealTimeClient', () => {
+  suite('createPortal', () => {
+    test('failure response from rest gateway', async () => {
+      const stubRestGateway = {
+        post: (url, body) => {
+          return Promise.resolve({ ok: false, body: {} })
+        }
+      }
+      const client = new RealTimeClient({restGateway: stubRestGateway})
+
+      let error
+      try {
+        await client.createPortal()
+      } catch (e) {
+        error = e
+      }
+      assert.equal(error.message, 'Portal creation failed')
+    })
+  })
+})
