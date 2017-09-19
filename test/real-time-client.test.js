12f2fff31c55301949eb0edbb67d8c9cbddd8088
Antonio Scandurra <as-cii@github.com>
2017-09-19 16:22:26 +0200

    Throw if retrieving client identity takes too long

--- test/real-time-client.test.js
+++ test/real-time-client.test.js
@@ -1,9 +1,28 @@
 require('./setup')
 const assert = require('assert')
-const {PortalCreationError} = require('../lib/errors')
+const Errors = require('../lib/errors')
 const RealTimeClient = require('../lib/real-time-client')
 
 suite('RealTimeClient', () => {
+  suite('initialize', () => {
+    test('waiting too long to retrieve the client id from the pub-sub gateway', async () => {
+      const pubSubGateway = {
+        getClientId () {
+          return new Promise(() => {})
+        }
+      }
+      const client = new RealTimeClient({pubSubGateway, timeoutInMilliseconds: 100})
+
+      let error
+      try {
+        await client.initialize()
+      } catch (e) {
+        error = e
+      }
+      assert(error instanceof Errors.PubSubConnectionError)
+    })
+  })
+
   suite('createPortal', () => {
     test('failure response from rest gateway', async () => {
       const stubRestGateway = {
@@ -19,7 +38,7 @@ suite('RealTimeClient', () => {
       } catch (e) {
         error = e
       }
-      assert(error instanceof PortalCreationError)
+      assert(error instanceof Errors.PortalCreationError)
     })
   })
 })
