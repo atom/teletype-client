49b9bf55603cd596b5f1c56ce115bc5f69035762
Antonio Scandurra <as-cii@github.com>
2017-09-26 09:26:30 +0200

    Simplify portal.test.js

--- test/portal.test.js
+++ test/portal.test.js
@@ -1,22 +1,32 @@
 const assert = require('assert')
 const Portal = require('../lib/portal')
 const {Disposable} = require('event-kit')
+const {startTestServer} = require('@atom/real-time-server')
+const {buildPeerPool, clearPeerPools} = require('./helpers/peer-pools')
 
 suite('Portal', () => {
+  let server
+
+  suiteSetup(async () => {
+    server = await startTestServer()
+  })
+
+  suiteTeardown(() => {
+    return server.stop()
+  })
+
+  setup(() => {
+    return server.reset()
+  })
+
+  teardown(() => {
+    clearPeerPools()
+  })
+
   suite('join', () => {
     test('throws and disposes itself when a network error occurs', async () => {
-      const stubPeerPool = {
-        onDisconnection () {
-          return new Disposable(() => {})
-        },
-        onReceive () {
-          return new Disposable(() => {})
-        },
-        getPeerIdentity () {
-          return {}
-        }
-      }
-      const portal = new Portal({id: 'id', hostPeerId: 'host', siteId: 2, peerPool: stubPeerPool})
+      const peerPool = await buildPeerPool('guest', server)
+      const portal = new Portal({id: 'id', hostPeerId: 'host', siteId: 2, peerPool})
       portal.network.connectTo = function () {
         throw new Error('an error')
       }
